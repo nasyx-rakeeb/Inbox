3b145804e0538f67ad770783198f9213bcec9fd6
Nasyx Rakeeb <nasyxrakeeb2@gmail.com>
2022-10-07 23:11:48 +0530

    first commit

---
+++ test/inbox.test.js
@@ -0,0 +1,28 @@
+const assert = require("assert")
+const ganache = require("ganache-cli")
+const Web3 = require("web3")
+const web3 = new Web3(ganache.provider())
+const {interface, bytecode} = require("../compile.js")
+
+let accounts
+let inbox
+
+beforeEach( async () => {
+  accounts = await web3.eth.getAccounts()
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({data: bytecode, arguments: ["Hello"]})
+      .send({from: accounts[0], gas: 1000000})
+})
+
+describe("Inbox", () => {
+  it("Deploys the inbox contract", () => {
+    //console.log(inbox)
+  })
+})
+
+
+
+
+
+
+
