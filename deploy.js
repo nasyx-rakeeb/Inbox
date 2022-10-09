a27849ef0b0e1a0e893ac91aa449418a876e92aa
Replit user <>
2022-10-09 15:56:32 +0000

    fux

--- deploy.js
+++ deploy.js
@@ -3,7 +3,7 @@ const Web3 = require("web3")
 const {interface, bytecode} = require("./compile.js")
 const phrase = "hollow attitude clinic tool middle business dice spread drift jaguar panel chest"
 const url = "https://goerli.infura.io/v3/9f935a0dec1f43d4aaa7b618ebbf0294"
-const provider = new HdWalletProvider(phrase, )
+const provider = new HdWalletProvider(phrase, url)
 const web3 = new Web3(provider)
 
 const deploy = async () => {
