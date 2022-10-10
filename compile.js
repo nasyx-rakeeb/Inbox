3bb0f18d070e74ebabcea0798dcaa680a00a2e8c
Replit user <>
2022-10-10 10:18:24 +0000

    minor

--- compile.js
+++ compile.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
-const path = require("path")
-const solc = require("solc")
+const path = require('path')
+const solc = require('solc')
 
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol")
 const source = fs.readFileSync(inboxPath, "utf8")
